9ee08521bfe05620f12288354cc58b58bad25eef
Elliot <apachehaisley@gmail.com>
2024-09-22 14:33:24 -0400

    update subproject versions

--- setup.py
+++ setup.py
@@ -10,9 +10,13 @@ basedir = 'data/meson'
 
 oracle_home = os.environ.get('ORACLE_HOME')
 
+meson_path = [ 'meson' ]
+if os.path.exists('K:/github/meson/meson.py'):
+    meson_path = [ 'python', 'K:/github/meson/meson.py' ]
+
 if __name__ == '__main__':
     args = [
-        'python3', 'K:/github/meson/meson.py', 'setup', builddir,
+        *meson_path, 'setup', builddir,
         '--native-file', f'{basedir}/base.ini',
         '--native-file', f'{basedir}/clang-cl.ini',
         '--native-file', f'{basedir}/{buildtype}.ini'
